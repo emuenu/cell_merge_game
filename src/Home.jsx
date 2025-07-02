f878b2cb4dfbcbe7895a670c8cc6f3fa19bf0382
emuenu <107606113+emuenu@users.noreply.github.com>
2025-07-03 03:11:00 +0900

    fix: CSSを修正

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import './style/App.css';
 // トップページ用コンポーネント（Homeにルーティングされると表示）
 function Home() {
     return (
-        <div class="home-layout">
+        <div className="home-layout">
             <p>Welcome to the Cell Merge Game!</p>
             <p>Click "Game Start" to begin, or "Rules" to check the rules.</p>
         </div>
