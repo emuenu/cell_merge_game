7413c6a3347d471c4c8cd1cf611ec941d84bd7f2
Merucu <cykps.ps@gmail.com>
2025-07-07 16:57:12 +0900

    style: add ignre dir

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,144 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation, useNavigate} from 'react-router-dom';
-import './style/App.css';
-import Home from './Home';
-import Game from './Game';
-import Rule from './Rule';
+import { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useLocation,
+    useNavigate,
+} from "react-router-dom";
+import "./style/App.css";
+import Home from "./Home";
+import Game from "./Game";
+import Rule from "./Rule";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Main />
-      </Router>
-    </div>
-  )
+    return (
+        <div>
+            <Router>
+                <Main />
+            </Router>
+        </div>
+    );
 }
 
 function Main() {
+    // それぞれの状態を定義（変更時に再レンダリングさせる）
+    const [vertical, setVertical] = useState("");
+    const [horizontal, setHorizontal] = useState("");
+    const [win_number, setWin_number] = useState("");
 
-  // それぞれの状態を定義（変更時に再レンダリングさせる）
-  const [vertical, setVertical] = useState('');
-  const [horizontal, setHorizontal] = useState('');
-  const [win_number, setWin_number] = useState('');
-  
-  // ページ遷移をスクリプトから管理するための変数navigateを取得
-  const navigate = useNavigate();
+    // ページ遷移をスクリプトから管理するための変数navigateを取得
+    const navigate = useNavigate();
 
-  // 現在いるページのパスを取得
-  const location = useLocation();
+    // 現在いるページのパスを取得
+    const location = useLocation();
 
-  // 現在のパスが「/」の時にisHomeがTrueになるようにする
-  const isHome = location.pathname === '/';
+    // 現在のパスが「/」の時にisHomeがTrueになるようにする
+    const isHome = location.pathname === "/";
 
-  // input入力時のイベントハンドラ関数
-  const handleVerticalInput = (e) => {
-    setVertical(e.target.value); // 入力値をverticalへセット
-  };
+    // input入力時のイベントハンドラ関数
+    const handleVerticalInput = (e) => {
+        setVertical(e.target.value); // 入力値をverticalへセット
+    };
 
-  const handleHorizontalInput = (e) => {
-    setHorizontal(e.target.value); // 入力値をhorizontalへセット
-  };
+    const handleHorizontalInput = (e) => {
+        setHorizontal(e.target.value); // 入力値をhorizontalへセット
+    };
 
-  const handleWin_numberInput = (e) => {
-    setWin_number(e.target.value); // 入力値をwin_numberへセット
-  };
+    const handleWin_numberInput = (e) => {
+        setWin_number(e.target.value); // 入力値をwin_numberへセット
+    };
 
-  // ゲームを開始するイベントハンドラ関数
-  const handleStartGame = () => {
+    // ゲームを開始するイベントハンドラ関数
+    const handleStartGame = () => {
+        if (vertical === "" || horizontal === "" || win_number === "") {
+            alert("ゲームを始めるには全ての項目に値を入力してください");
+            return; // 遷移を止める
+        }
 
-    if (vertical === '' || horizontal === '' || win_number === '') {
-      alert('ゲームを始めるには全ての項目に値を入力してください');
-      return; // 遷移を止める
-    }
+        // typeをnumber（文字列）としてinputしたので、数値に変換する
+        const v = Number(vertical);
+        const h = Number(horizontal);
+        const w = Number(win_number);
 
-    // typeをnumber（文字列）としてinputしたので、数値に変換する
-    const v = Number(vertical);
-    const h = Number(horizontal);
-    const w = Number(win_number);
+        if (v < w && h < w) {
+            alert(
+                "縦横のマス数は、どちらかが揃えるマス数以上の値である必要があります",
+            );
+            return; // 遷移を止める
+        }
 
-    if (v < w && h < w) {
-      alert('縦横のマス数は、どちらかが揃えるマス数以上の値である必要があります');
-      return; // 遷移を止める
-    }
+        if (v < 3 || h < 3 || w < 3) {
+            alert("各値は3以上の数に設定してください");
+            return; // 遷移を止める
+        }
 
-    if (v < 3 || h < 3 || w < 3) {
-      alert('各値は3以上の数に設定してください');
-      return; // 遷移を止める
-    }
-    
-    // Game.jsx（/game）にページ遷移させる
-    // vertical(v), horizontal(h), win_number(w)をpropsとして渡す
-    navigate('/game', {state: { v, h, w }});
+        // Game.jsx（/game）にページ遷移させる
+        // vertical(v), horizontal(h), win_number(w)をpropsとして渡す
+        navigate("/game", { state: { v, h, w } });
+    };
 
-  };
+    // ルールを表示するイベントハンドラ関数
+    const handleRules = () => {
+        navigate("/rule"); // Rule.jsx（/rule）にページ遷移させる
+    };
 
-  // ルールを表示するイベントハンドラ関数
-  const handleRules = () => {
-    navigate('/rule'); // Rule.jsx（/rule）にページ遷移させる
-  };
+    // 数値を入力してゲーム開始とルール説明を見る
+    return (
+        <div className="home-layout">
+            {/* isHomeがTrueの時（'/'にいる時）のみ以下は表示させる */}
+            {isHome && (
+                <div>
+                    <p>Welcome</p>
 
-  // 数値を入力してゲーム開始とルール説明を見る
-  return (
-    <div className="home-layout">
+                    <div className="inputs">
+                        <label>
+                            縦:{" "}
+                            <input
+                                type="number"
+                                min="3"
+                                value={vertical}
+                                onChange={handleVerticalInput}
+                            />
+                        </label>
+                        <label>
+                            横:{" "}
+                            <input
+                                type="number"
+                                min="3"
+                                value={horizontal}
+                                onChange={handleHorizontalInput}
+                            />
+                        </label>
+                        <label>
+                            何文字揃えたら勝利か:{" "}
+                            <input
+                                type="number"
+                                min="3"
+                                value={win_number}
+                                onChange={handleWin_numberInput}
+                            />
+                        </label>
+                    </div>
 
-      {/* isHomeがTrueの時（'/'にいる時）のみ以下は表示させる */}
-      {isHome && (
+                    {/* ボタンでイベントハンドラ関数を呼んで遷移制御を行う */}
+                    <div className="button-group">
+                        <button onClick={handleStartGame}>Game Start</button>
+                        <button onClick={handleRules}>Rules</button>
+                    </div>
+                </div>
+            )}
 
-        <div>
-
-          <p>Welcome</p>
-
-          <div className="inputs">
-            <label>
-              縦: <input type="number" min='3' value={vertical} onChange={handleVerticalInput} />
-            </label>
-            <label>
-              横: <input type="number" min='3' value={horizontal} onChange={handleHorizontalInput} />
-            </label>
-            <label>
-              何文字揃えたら勝利か: <input type="number" min='3' value={win_number} onChange={handleWin_numberInput} />
-            </label>
-          </div>
-
-          {/* ボタンでイベントハンドラ関数を呼んで遷移制御を行う */}
-          <div className='button-group'>
-            <button onClick={handleStartGame}>Game Start</button>
-            <button onClick={handleRules}>Rules</button>
-          </div>
-
+            {/* 各パスに関してのルーティング設定 */}
+            <Routes>
+                {/* ルートディレクトリにはHomeをルーティングする */}
+                <Route path="/" element={<Home />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/rule" element={<Rule />} />
+            </Routes>
         </div>
-
-      )}
-
-      {/* 各パスに関してのルーティング設定 */}
-      <Routes>
-        {/* ルートディレクトリにはHomeをルーティングする */}
-        <Route path="/" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/rule" element={<Rule />} />
-      </Routes>
-
-    </div>
-
-  );
-
+    );
 }
 
 export default App;
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import GameBoard from './GameBoard';
-import './style/Game.css';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import GameBoard from "./GameBoard";
+import "./style/Game.css";
 
 function Game() {
-
     // 縦がverticalで横がhorizontalのマス目用の二次元配列を用意する
     // セルのクリックで対応する要素に1(O)か2(X)を入れるようにする
     // 0: 空、1: X→player: 1, 2: O→player: 2（最初は全て0）
-    // -1: ←(c-1)を参照, -2: ←(r-1)を参照 
+    // -1: ←(c-1)を参照, -2: ←(r-1)を参照
 
     const location = useLocation();
     const { v, h, w } = location.state || {}; // 数値型に変換済みで送られてくるv、h、wを受け取る
@@ -21,7 +20,7 @@ function Game() {
     const [mergeColIndex_V, setMergeColIndex_V] = useState(null);
     const [mergeRowIndex_H, setMergeRowIndex_H] = useState(null); // 乱数を使って決める横結合位置
     const [mergeColIndex_H, setMergeColIndex_H] = useState(null);
-    
+
     // 名前解決
     const vertical = v;
     const horizontal = h;
@@ -36,12 +35,11 @@ function Game() {
 
     // tableを初期化してマス目を生成する
     useEffect(() => {
-
         if (!vertical || !horizontal) return; // verticalやhorizontalがundefinedなら何もしない
-        
+
         // vとhを用いて、二次元配列を生成する（ゲーム盤を初期化）
         const newTable = Array.from({ length: vertical }, () =>
-            Array(horizontal).fill(0)
+            Array(horizontal).fill(0),
         );
         setTable(newTable); // tableを更新
 
@@ -49,62 +47,73 @@ function Game() {
         let mergeRowH, mergeColH; // 横方向結合位置（行数がmergeRowHで列数がmergeColH）
 
         // 乱数を生成
-        while(true){
-
+        while (true) {
             mergeRowV = getRandomInt(0, vertical - 2); // 下に飛び出してはいけないのでvertical-2まで
             mergeColV = getRandomInt(0, horizontal - 1);
             mergeRowH = getRandomInt(0, vertical - 1);
             mergeColH = getRandomInt(0, horizontal - 2); // 右に飛び出してはいけないのでhorizontal-2まで
 
             // 値が適切でない場合は乱数生成をやり直す
-            if(
-                !(mergeRowV === mergeRowH && (mergeColV === mergeColH || mergeColV === mergeColH + 1)) &&  // 横方向の結合セルのどちらか2つから下に縦方向の結合セルを伸ばすのはダメ
-                !(mergeRowV === mergeRowH - 1 && (mergeColV === mergeColH || mergeColV === mergeColH + 1)) && // 縦方向の結合セルが横方向の結合セルと被るのはダメ
-                !(mergeColH === mergeColV && (mergeRowH === mergeRowV || mergeRowH === mergeRowV + 1)) && // 縦方向の結合セルのどちらか2つから右に横方向の結合セルを伸ばすのはダメ
-                !(mergeColH === mergeColV -1 && (mergeRowH === mergeRowV || mergeRowH === mergeRowV + 1)) // 横方向の結合セルが縦方向の結合セルと被るのはダメ
-            ) break;
-
+            if (
+                !(
+                    mergeRowV === mergeRowH &&
+                    (mergeColV === mergeColH || mergeColV === mergeColH + 1)
+                ) && // 横方向の結合セルのどちらか2つから下に縦方向の結合セルを伸ばすのはダメ
+                !(
+                    mergeRowV === mergeRowH - 1 &&
+                    (mergeColV === mergeColH || mergeColV === mergeColH + 1)
+                ) && // 縦方向の結合セルが横方向の結合セルと被るのはダメ
+                !(
+                    mergeColH === mergeColV &&
+                    (mergeRowH === mergeRowV || mergeRowH === mergeRowV + 1)
+                ) && // 縦方向の結合セルのどちらか2つから右に横方向の結合セルを伸ばすのはダメ
+                !(
+                    mergeColH === mergeColV - 1 &&
+                    (mergeRowH === mergeRowV || mergeRowH === mergeRowV + 1)
+                ) // 横方向の結合セルが縦方向の結合セルと被るのはダメ
+            )
+                break;
         }
-        
+
         // 値を設定
         setMergeRowIndex_V(mergeRowV);
         setMergeColIndex_V(mergeColV);
         setMergeRowIndex_H(mergeRowH);
         setMergeColIndex_H(mergeColH);
-        
     }, [vertical, horizontal]); // verticalとhorizontalに依存する
 
     // 手番が終わった時、そのプレイヤーが勝利しているか勝利判定を行うアルゴリズム
-  
+
     // -1, -2を処理して、0か1か2を返す関数
     const getStatus = (table, r, c) => {
-      if (table[r][c] == 0) {
-        return 0;
-      }
-      if (table[r][c] == 1) {
-        return 1;
-      }
-      if (table[r][c] == 2) {
-        return 2;
-      }
-      if (table[r][c] == -1) {
-        return table[r][c-1];
-      }
-      if (table[r][c] == -2) {
-        return table[r-1][c];
-      }
-    }
+        if (table[r][c] == 0) {
+            return 0;
+        }
+        if (table[r][c] == 1) {
+            return 1;
+        }
+        if (table[r][c] == 2) {
+            return 2;
+        }
+        if (table[r][c] == -1) {
+            return table[r][c - 1];
+        }
+        if (table[r][c] == -2) {
+            return table[r - 1][c];
+        }
+    };
 
     // 横の列をチェック
     const checkRows = (table, win_number, currentPlayer) => {
-
         // table.lengthは縦の行数、table[0].lengthは横の列数になる
         for (let r = 0; r < table.length; r++) {
             let count = 0;
             for (let c = 0; c <= table[0].length; c++) {
-
                 let v = table[r][c];
-                if (v == currentPlayer || (v == -2 && table[r-1][c] == currentPlayer)) {
+                if (
+                    v == currentPlayer ||
+                    (v == -2 && table[r - 1][c] == currentPlayer)
+                ) {
                     count += 1;
                 } else if (v == -1) {
                     // なにもしない
@@ -120,13 +129,15 @@ function Game() {
 
     // 縦の列をチェック
     const checkColumns = (table, win_number, currentPlayer) => {
-
         // table.lengthは縦の行数、table[0].lengthは横の列数になる
         for (let c = 0; c < table.length; c++) {
             let count = 0;
             for (let r = 0; r < table[0].length; r++) {
                 let v = table[r][c];
-                if (v == currentPlayer || (v == -1 && table[r][c-1] == currentPlayer)) {
+                if (
+                    v == currentPlayer ||
+                    (v == -1 && table[r][c - 1] == currentPlayer)
+                ) {
                     count += 1;
                 } else if (v == -2) {
                     // なにもしない
@@ -143,64 +154,63 @@ function Game() {
     // 斜めをチェック
     const checkDiagonals = (table, win_number, currentPlayer) => {
         // 「⇗」と「⇙」の斜めをチェック
-        for (let i = 0; i <= table.length + table[0].length -2; i++) {
+        for (let i = 0; i <= table.length + table[0].length - 2; i++) {
             let count = 0;
             for (let r = 0; r < table.length && r <= i; r++) {
                 let c = i - r;
                 console.log(r, c);
-                if (getStatus(table, r, c) == currentPlayer){
-                  count++;
+                if (getStatus(table, r, c) == currentPlayer) {
+                    count++;
                 } else {
-                  count = 0;
+                    count = 0;
                 }
 
                 // win_number分だけ連続した数が見つかればtrue
                 if (count === win_number) return true;
             }
-
         }
 
         // 「⇘」と「⇖」の斜めをチェック
-        for (let i = 0; i <= table.length + table[0].length -2; i++) {
+        for (let i = 0; i <= table.length + table[0].length - 2; i++) {
             let count = 0;
             for (let r = 0; r < table.length && r <= i; r++) {
                 let c = table[0].length - (i - r);
                 console.log(r, c);
-                if (getStatus(table, r, c) == currentPlayer){
-                  count++;
+                if (getStatus(table, r, c) == currentPlayer) {
+                    count++;
                 } else {
-                  count = 0;
+                    count = 0;
                 }
 
                 // win_number分だけ連続した数が見つかればtrue
                 if (count === win_number) return true;
             }
-
         }
 
         return false;
-
     };
 
     // その時のマス目の状態と手番のプレイヤーに対応して勝利判定を行う（勝ったらtrue）
     const wasWin = (table, win_number, currentPlayer) => {
         // 縦、横、斜めのいずれかでtrueになっていれば勝利としてtrueを返す
-        if (checkRows(table, win_number, currentPlayer) || 
-            checkColumns(table, win_number, currentPlayer) || 
-            checkDiagonals(table, win_number, currentPlayer))  return true;
+        if (
+            checkRows(table, win_number, currentPlayer) ||
+            checkColumns(table, win_number, currentPlayer) ||
+            checkDiagonals(table, win_number, currentPlayer)
+        )
+            return true;
         return false;
     };
 
     // セルがクリックされたときのイベントハンドラ関数
     const handleCellClick = (row, col) => {
-
         if (table[row][col] !== 0) return; // 既にOかXが入力されていれば無視
-        
+
         // クリックした位置のセルに対応する配列の要素に1(O)か2(X)を入れた新しい配列newTableを作成
         const newTable = table.map((r, rowIndex) =>
             r.map((cell, colIndex) =>
-                rowIndex === row && colIndex === col ? currentPlayer : cell
-            )
+                rowIndex === row && colIndex === col ? currentPlayer : cell,
+            ),
         );
 
         // ゲーム盤を更新させる
@@ -214,15 +224,15 @@ function Game() {
         // その時、表示されなくなり使われなくなった結合された側のセル（右か下）の位置には配列としては横結合なら-1、縦結合なら-2を入れておく
 
         // 勝利判定を行う（win_numberは数値に型変換する）
-        if(wasWin(newTable, win_number, currentPlayer)) {
+        if (wasWin(newTable, win_number, currentPlayer)) {
             alert(`Player ${currentPlayer} wins!`);
-            navigate("/");  // ホームにリダイレクトさせる
+            navigate("/"); // ホームにリダイレクトさせる
             return; // ゲームは終了
         }
         console.log("asda");
 
         // 引き分け判定（すべてのセルが埋まっているか）
-        const isDraw = newTable.every(row => row.every(cell => cell !== 0));
+        const isDraw = newTable.every((row) => row.every((cell) => cell !== 0));
 
         // 引き分けの場合もホームにリダイレクトさせてゲーム終了
         if (isDraw) {
@@ -233,14 +243,16 @@ function Game() {
 
         // （勝利していない場合）プレイヤーの手番を次へ交代させる
         setCurrentPlayer(currentPlayer === 1 ? 2 : 1);
-
     };
 
     // GameBoardコンポーネントへpropsに渡す
     // 二次元配列に対応させてHTMLのテーブルとしてO、Xの表示を含めてゲーム盤を表示させる
     return (
-        <div className='game-layout'>
-            <p>縦: {vertical} / 横: {horizontal} / 勝利条件: {win_number} マス揃える</p>
+        <div className="game-layout">
+            <p>
+                縦: {vertical} / 横: {horizontal} / 勝利条件: {win_number}{" "}
+                マス揃える
+            </p>
             <GameBoard
                 table={table}
                 mergeRowIndex_V={mergeRowIndex_V}
@@ -252,7 +264,6 @@ function Game() {
             <Link to="/">Back to Home</Link>
         </div>
     );
-
 }
 
 export default Game;
